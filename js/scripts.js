3f1433cb7d729cc761cbde4233e87eff38f11c55
3Emme <emmettbuentiempo@gmail.com>
2020-08-14 15:38:28 -0700

    add hide/show logic for answers 1-3 dependent on the data collected from form programming-lang-quiz, including hiding of the form after it has been submitted. Add id aspect to quiz form element.

--- js/scripts.js
+++ js/scripts.js
@@ -60,4 +60,24 @@ $(document).ready(function() {
     event.preventDefault()
   });
 
+  $("form#programming-lang-quiz").submit(function(event) {
+    const q1 = $("select#q1").val();
+    const q2 = $("select#q2").val();
+    const q3 = $("input[name='quiz-question-3-radios']:checked").val();
+    const q4 = $("input[name='quiz-question-4-radios']:checked").val();
+    const q5 = $("input[name='quiz-question-5-radios']:checked").val();
+    
+    $("#quiz-row").hide();
+    
+    if (q5 === "q5a1"){
+      $("#answer-card-1").show();
+      } else if (q5 === "q5a2"){
+      $("#answer-card-2").show();
+      } else if (q5 === "q5a3"){
+      $("#answer-card-3").show();
+    }
+
+    event.preventDefault();
+  });
+
 });
