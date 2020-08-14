f191859e949808e89f65462929d890c92a847086
3Emme <emmettbuentiempo@gmail.com>
2020-08-14 15:07:59 -0700

    replicate initial hide show logic from question row 1 onto row 2, and add appropriate hide show logic for radio buttons onto question row 3.

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,52 @@ $(document).ready(function() {
     event.preventDefault()
   });
 
+  $("select#q2").change(function(event) {
+    const q2 = $("select#q2").val();
+
+    if (q2 === "q2a1"|| q2 === "q2a2"){
+      $("#quiz-question-3-row").show();
+      } else {
+      $("#quiz-question-3-row").hide();
+      }
+
+    event.preventDefault()
+  });
+
+  $("input[name='quiz-question-3-radios']").change(function(event) {
+    const q3 = $("input[name='quiz-question-3-radios']:checked").val();
+
+    if (q3 === "q3a1"|| q3 === "q3a2"|| q3 === "q3a3"){
+      $("#quiz-question-4-row").show();
+      } else {
+      $("#quiz-question-4-row").hide();
+      }
+
+    event.preventDefault()
+  });
+
+  $("select#q1").change(function(event) {
+    const q1 = $("select#q1").val();
+
+    if (q1 === "q1a1"|| q1 === "q1a2"){
+      $("#quiz-question-2-row").show();
+      } else {
+      $("#quiz-question-2-row").hide();
+      }
+
+    event.preventDefault()
+  });
+
+  $("select#q1").change(function(event) {
+    const q1 = $("select#q1").val();
+
+    if (q1 === "q1a1"|| q1 === "q1a2"){
+      $("#quiz-question-2-row").show();
+      } else {
+      $("#quiz-question-2-row").hide();
+      }
+
+    event.preventDefault()
+  });
+
 });
